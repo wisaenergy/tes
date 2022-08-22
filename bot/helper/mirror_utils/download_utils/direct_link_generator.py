ef6d02fafa8c5da98288394e264e171d2de2ea71
Aenergy <109235736+wisaenergy@users.noreply.github.com>
2022-08-23 03:06:40 +0700

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -15,7 +15,7 @@ from json import loads as jsonloads
 from lk21 import Bypass
 from cfscrape import create_scraper
 from bs4 import BeautifulSoup
-from base64 import standard_b64encode, b64decode
+from base64 import b64decode, standard_b64encode
 from time import sleep
 
 from bot import LOGGER, UPTOBOX_TOKEN, CRYPT
